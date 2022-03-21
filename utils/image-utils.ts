a7db7be5b1cdb2d013acf7443c13a8883ab0c8bd
Artem Pakk <apakk@me.com>
2022-03-21 19:37:51 +0200

    Image utils

---
+++ utils/image-utils.ts
@@ -0,0 +1,41 @@
+import ExifReader from "exifreader";
+import fs from "fs";
+import { IImage } from "../interfaces/image";
+
+const IMAGES_DIR = "public/img";
+
+export const buildImageWithMetadata = (filename: string): IImage => {
+  const buffer = fs.readFileSync(`${IMAGES_DIR}/${filename}`);
+  const { exif, iptc } = ExifReader.load(buffer, { expanded: true });
+
+  return {
+    filename,
+    slug: filename.replace('.jpg', ''),
+    title: iptc?.['Object Name']?.description,
+    caption: exif?.ImageDescription?.description,
+    gpsLatitude: exif?.GPSLatitude?.description,
+    gpsLongitude: exif?.GPSLongitude?.description,
+    country: iptc?.['Country/Primary Location Name']?.description,
+    state: iptc?.['Province/State']?.description,
+    city: iptc?.City?.description,
+    sublocation: iptc?.['Sub-location']?.description,
+    cameraMake: exif?.Make?.description,
+    cameraModel: exif?.Model?.description,
+    lensMake: exif?.LensMake?.description,
+    lensModel: exif?.LensModel?.description,
+    focalLength: exif?.FocalLength?.description,
+    focalLengthIn35mmFilm: exif?.FocalLengthIn35mmFilm?.description,
+    shutterSpeed: exif?.ShutterSpeedValue?.description,
+    apertureValue: exif?.ApertureValue?.description,
+    capturedAt: `${iptc?.['Date Created']?.description} ${iptc?.['Time Created']?.description}`
+  }
+};
+
+export const getAllImages = (): IImage[] => {
+  const files = fs.readdirSync(IMAGES_DIR);
+  const images = files.map((filename) => {
+    return buildImageWithMetadata(filename);
+  });
+
+  return images;
+}
