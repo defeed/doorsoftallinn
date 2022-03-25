74602385981dd3a7ef43951c726b129536b56305
Artem Pakk <apakk@me.com>
2022-03-25 17:18:51 +0200

    Remove BackIcon from slug

--- pages/door/[slug].tsx
+++ pages/door/[slug].tsx
@@ -55,23 +55,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-function BackIcon(props: any) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M10 19l-7-7m0 0l7-7m-7 7h18"
-      />
-    </svg>
-  );
-}
-
 export default Door;
